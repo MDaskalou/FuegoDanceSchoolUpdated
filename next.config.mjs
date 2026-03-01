1335dc57cf62e319b35083ed6c878cee37262700
Mikael Daskalou <mikael.daskalou@hotmail.com>
2026-03-01 13:29:20 +0100

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,25 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // --- PRODUKTION / BUILD INSTÄLLNINGAR ---
-    // Avkommentera dessa rader INNAN du kör 'npm run build'
-
-    // output: 'export',
-    // basePath: '/FuegoDanceSchoolUpdated',
-
-
     reactStrictMode: true,
     trailingSlash: true,
 
+    // Lägg till detta block:
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/sv/', // Notera snedstrecket i slutet pga trailingSlash: true
+                permanent: true,
+            },
+        ]
+    },
+
     images: {
-        // Denna del behövs för att Next.js ska lita på bilderna från Behold & Instagram
-        // Det behövs BÅDE lokalt och i produktion (om du inte kör unoptimized)
         remotePatterns: [
             { protocol: 'https', hostname: 'behold.pictures' },
             { protocol: 'https', hostname: 'scontent-*.cdninstagram.com' },
             { protocol: 'https', hostname: '*.cdninstagram.com' },
         ],
-
-        // Avkommentera denna rad INNAN du kör 'npm run build' för Static Export
-        // unoptimized: true,
     },
 };
 
